41c309f06736f5d91c7a69167a8872f55947f835
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2022-10-28 22:06:42 +0300

    first commit

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,11 +1,20 @@
 class Magazine
   attr_accessor :name, :category
+  @@all=[]
 
+  def self.all
+    @@all
+  end
 
   def initialize(name, category)
     @name = name
     @category = category
+    @@all << self
+    
 
+  end
+  def contributers
+    
   end
 
 
