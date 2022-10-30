645c8081acb4c7374a80aa5483d33c45de9c507c
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2022-10-30 16:29:30 +0300

    used name to find specific magazine

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -16,6 +16,10 @@ class Magazine
   def contributors
     Article.all.filter{|article|article.magazine.name==@name}.map{|article|article.author}.uniq
   end
+  def self.find_by_name(name)
+    Magazine.all.find{|magazine|
+      magazine.name == name}
+  end
 
 
 end
