ce0cb5ebe5250bebdcf920a9d86a2b048e455288
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2022-10-30 15:58:41 +0300

    Added test code

--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,16 @@ end
 
 
 ###  WRITE YOUR TEST CODE HERE ###
+author=Author.new("Shakespear")
+magazine1=Magazine.new("The Forbes","Fashion")
+magazine2=Magazine.new("Peoples Daily","Politics")
+magazine3=Magazine.new("Breaking news","Politics")
+article1=Article.new(author,magazine1,"The title")
+article2=Article.new(author,magazine2,"The new title")
+article3=Article.new(author,magazine1,"Just title")
+author.add_article(magazine1,"another title")
+author.add_article(magazine2,"newest title")
+author.add_article(magazine3,"another new title")
 
 
 
